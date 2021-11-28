752773ec9a33203ee1772891b1fd7a34db474cab
mohamadabdallah4 <mfa68@mail.aub.edu>
2021-11-28 16:42:59 +0200

    Pass data to attendance page

--- src/components/widgets/AttendanceWidget.jsx
+++ src/components/widgets/AttendanceWidget.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 import Students from "../../Students";
 import StudentCard from "./StudentCard";
-// import { useLocation } from "react-router-dom";
 
 function createStudentCard(student) {
   return <StudentCard name={student.studentName} mode="Normal" />;
 }
 
 function AttendanceWidget(props) {
-  // const location = useLocation();
-  // const { data } = location.state;
   return (
     <div className="dash-container">
       <div id="lower-section">
         {/* <h1 className="title">{data} - Taking Attendance - Session 12</h1> */}
-        <h1 className="title">Taking Attendance</h1>
+        <h1 className="title">{props.data} - Taking Attendance</h1>
         <div id="card-section">{Students.map(createStudentCard)}</div>
       </div>
     </div>
