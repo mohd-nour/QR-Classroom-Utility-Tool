a740a56d12862480f036a75ea2135d2ca0f8d17a
mohamadabdallah4 <mfa68@mail.aub.edu>
2022-01-11 22:32:17 +0200

    still work on forgot pass

--- server/controllers/passwordReset.js
+++ server/controllers/passwordReset.js
@@ -6,11 +6,16 @@ export const sendEmail = async (req,res) => {
     const { email } = req.body;
     const existingUser = await User.findOne({ email });
     const userId = existingUser._id;
+    const name = existingUser.name;
+    const tokenEntry = await Token.findOne({id: userId});
+    const token = tokenEntry.token;
     if (!existingUser) {return res.status(404).json({message:"User does not exist."});}
-    console.log(existingUser);
-    console.log(userId+"test");
+    console.log(userId);
+    console.log(token);
+    const link = "http://localhost:5000/"+userId+"/"+token;
     try {
-        mailSend(email,"Reset password", "Hello! this is a test mail");
+        const message = "Hello "+name+"\n If you want to reset your password, please click the link below:\n"+link;
+        mailSend(email,"Reset password", message);
     } catch (error) {
         res.status(500).json({message:"Something went wrong!"});
     }
