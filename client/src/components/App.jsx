f20556fdc67accb23b590c53ffc7f019c597fdb1
Nour El Arab <muhammadalarab@gmail.com>
2022-03-12 05:16:57 +0200

    create landing page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,32 +1,34 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import SelectionPage from "./SelectionPage";
-import GradeReportPage from "./GradeReportPage";
-import AttendancePage from "./AttendancePage";
-import EnrollmentPage from "./EnrollmentPage.jsx";
-import LoginPage from "./LoginPage";
-import RegisterPage from "./RegisterPage";
-import AddClassPage from "./AddClassPage";
-import SendEmailForgotPassword from "./SendEmailForgotPassword";
-import ResetPassword from "./ResetPassword";
-import SessionsPage from "./SessionsPage";
-import AlertsPage from "./AlertsPage";
-import GradeSheets from "./GradeSheetsPage";
-import FinalizedAttendancePage from "./FinalizedAttendancePage";
-import Verified from "./Verified";
-import NewGradeSheetPage from "./NewGradeSheetPage";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './LandingPage.jsx';
+import Home from './Home';
+import SelectionPage from './SelectionPage';
+import GradeReportPage from './GradeReportPage';
+import AttendancePage from './AttendancePage';
+import EnrollmentPage from './EnrollmentPage.jsx';
+import LoginPage from './LoginPage';
+import RegisterPage from './RegisterPage';
+import AddClassPage from './AddClassPage';
+import SendEmailForgotPassword from './SendEmailForgotPassword';
+import ResetPassword from './ResetPassword';
+import SessionsPage from './SessionsPage';
+import AlertsPage from './AlertsPage';
+import GradeSheets from './GradeSheetsPage';
+import FinalizedAttendancePage from './FinalizedAttendancePage';
+import Verified from './Verified';
+import NewGradeSheetPage from './NewGradeSheetPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/LoginPage" element={<LoginPage />} />
         <Route path="/RegisterPage" element={<RegisterPage />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/Selection" element={<SelectionPage />} />
         <Route path="/Gradebook" element={<GradeReportPage />} />
-        <Route path="/GradeSheets" element={<GradeSheets/>} />
+        <Route path="/GradeSheets" element={<GradeSheets />} />
         <Route path="/Attendance/:sessionNumber" element={<AttendancePage />} />
         <Route path="/Enroll" element={<EnrollmentPage />} />
         <Route path="/AddClassPage" element={<AddClassPage />} />
---
+++ client/src/components/LandingPage.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Sensei from '../images/Sensei.png';
+import { Link } from 'react-router-dom';
+
+function LandingPage() {
+  return (
+    <div id="landing-page">
+      <nav className="landing-header">
+        <span className="sensei-landing-logo">Sensei</span>
+      </nav>
+      <div className="landing-panel">
+        <div class="landing-text">
+          <h1 class="landing-title">Welcome to Sensei</h1>
+          <h3 class="landing-sub-title">
+            A University Utility Software designed to revolutionize the
+            classroom.
+          </h3>
+
+          <Link to="/LoginPage" className="removeUnderline">
+            <div class="landing-button">Get Started</div>
+          </Link>
+        </div>
+        <img className="landing-image" src={Sensei} alt="Sensei Logo" />
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
